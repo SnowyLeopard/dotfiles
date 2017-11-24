a0159607f9124bb06c8fa755523c35dcaa92d1d5
SnowyLeopard <ricklucassen@gmail.com>
2017-11-24 10:22:24 +0100

    Added rvm install

--- install.sh
+++ install.sh
@@ -16,6 +16,13 @@ echo "Installing pip packages"
 echo "********************"
 pip install powerline-status --user
 
+# Install RVM
+echo "********************"
+echo "Installing RVM"
+echo "********************"
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # Install and compile vim
 if [[ ! -f "/usr/local/bin/vim" ]]
